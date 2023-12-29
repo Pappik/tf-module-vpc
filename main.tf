4b9b8dd65a70fc011074779babe3a5b137b28023
pappik <pappik@gmail.com>
2023-12-28 21:58:11 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -12,3 +12,22 @@ resource "aws_subnet" "main" {
   tags = merge(local.common_tags, { Name = "${var.env}-subnet-${count.index+1}"} )
 
 }
+
+resource "aws_vpc_peering_connection" "peering" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept   = true
+}
+
+resource "aws_route" "default" {
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = data.aws_vpc.default.id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
