e272683f3124c9c2d7c437f6b0eced95eb34cb2f
pappik <pappik@gmail.com>
2024-01-02 17:46:59 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -4,12 +4,21 @@ resource "aws_vpc" "main" {
   tags = merge(local.common_tags, { Name = "${var.env}-vpc"} )
 }
 
-resource "aws_subnet" "main" {
-  count      = length(var.subnets_cidr)
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnets_cidr[count.index]
+  cidr_block = var.public_subnets_cidr[count.index]
 
-  tags = merge(local.common_tags, { Name = "${var.env}-subnet-${count.index+1}"} )
+  tags = merge(local.common_tags, { Name = "${var.env}-public_subnet-${count.index+1}"} )
+
+}
+
+resource "aws_subnet" "private" {
+  count      = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets_cidr[count.index]
+
+  tags = merge(local.common_tags, { Name = "${var.env}-private_subnet-${count.index+1}"} )
 
 }
 
@@ -18,16 +27,14 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
+
+  tags = merge(local.common_tags, { Name = "${var.env}-peering"} )
+
 }
 
-resource "aws_route" "default" {
-  route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "172.31.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
-resource "aws_route" "default-vpc" {
-  route_table_id            = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block    = var.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+  tags = merge(local.common_tags, { Name = "${var.env}-igw"} )
+
 }
