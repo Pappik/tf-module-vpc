9d03a5be2834683d6469fa332ea5d60d295fc1da
pappik <pappik@gmail.com>
2024-01-02 22:13:04 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -14,20 +14,3 @@ resource "aws_vpc_peering_connection" "peering" {
 
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(local.common_tags, { Name = "${var.env}-igw"} )
-
-}
-
-#resource "aws_eip" "eip" {
-#   domain   = "vpc"
-#}
-#
-#resource "aws_nat_gateway" "ngw" {
-#  allocation_id = aws_eip.eip.id
-#  subnet_id     = aws_subnet.public.*.id[0]
-#
-#  tags = merge(local.common_tags, { Name = "${var.env}-natgw"} )
-#}
