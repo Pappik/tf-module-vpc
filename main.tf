1cedada460d927c30b4075c39b521a1b411dc3d9
pappik <pappik@gmail.com>
2024-01-02 19:55:54 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -4,27 +4,6 @@ resource "aws_vpc" "main" {
   tags = merge(local.common_tags, { Name = "${var.env}-vpc"} )
 }
 
-resource "aws_subnet" "public" {
-  count      = length(var.public_subnets_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnets_cidr[count.index]
-  availability_zones = var.availability_zones[count.index]
-
-  tags = merge(local.common_tags, { Name = "${var.env}-public_subnet-${count.index+1}"} )
-
-}
-
-resource "aws_subnet" "private" {
-  count      = length(var.private_subnets_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnets_cidr[count.index]
-  availability_zones = var.availability_zones[count.index]
-
-
-  tags = merge(local.common_tags, { Name = "${var.env}-private_subnet-${count.index+1}"} )
-
-}
-
 resource "aws_vpc_peering_connection" "peering" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = var.default_vpc_id
@@ -42,35 +21,6 @@ resource "aws_internet_gateway" "igw" {
 
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  route {
-    cidr_block                = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-  }
-  tags = merge(local.common_tags, { Name = "${var.env}-public_route_table"} )
-
-}
-
-  resource "aws_route_table_association" "public-rt-assoc" {
-    count          = length(aws_subnet.public)
-    subnet_id      = aws_subnet.public.*.id[count.index]
-    route_table_id = aws_route_table.public
-  }
-
-resource "aws_route" "default" {
-  route_table_id = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block = var.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-}
-
-
 resource "aws_eip" "eip" {
    domain   = "vpc"
 }
@@ -80,26 +30,4 @@ resource "aws_nat_gateway" "ngw" {
   subnet_id     = aws_subnet.public.*.id[0]
 
   tags = merge(local.common_tags, { Name = "${var.env}-natgw"} )
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw.id
-  }
-
-  route {
-    cidr_block                = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-  }
-  tags = merge(local.common_tags, { Name = "${var.env}-private_route_table"} )
-
-}
-
-resource "aws_route_table_association" "private-rt-assoc" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private.*.id[count.index]
-  route_table_id = aws_route_table.private
 }
--- vars.tf
+++ vars.tf
@@ -1,7 +1,3 @@
 variable "cidr_block" {}
 variable "env" {}
-variable "public_subnets_cidr" {}
-variable "private_subnets_cidr" {}
-variable "availability_zones" {}
-
 variable "default_vpc_id" {}
