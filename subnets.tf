92c74b8a45a7c1755fd174e67cf1177a2ad37601
pappik <pappik@gmail.com>
2024-01-03 16:06:26 -0500

    vpc terraform

--- subnets.tf
+++ subnets.tf
@@ -1,22 +1,22 @@
-module "subnet" {
-  source = "./subnets"
-
-  availability_zone = var.availability_zone
-  default_vpc_id = var.default_vpc_id
-  env = var.env
-
-  for_each = var.subnets
-  cidr_block = each.value.cidr_block
-  name       = each.value.name
-  internet_gw = lookup(each.value, "internet_gw", false )
-#  nat_gw = lookup(each.value, "nat_gw", false )
-
-
-  vpc_id = aws_vpc.main.id
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-  tags                      = local.common_tags
-  gateway_id                = aws_internet_gateway.igw.id
-}
+#module "subnet" {
+#  source = "./subnets"
+#
+#  availability_zone = var.availability_zone
+#  default_vpc_id = var.default_vpc_id
+#  env = var.env
+#
+#  for_each = var.subnets
+#  cidr_block = each.value.cidr_block
+#  name       = each.value.name
+#  internet_gw = lookup(each.value, "internet_gw", false )
+##  nat_gw = lookup(each.value, "nat_gw", false )
+#
+#
+#  vpc_id = aws_vpc.main.id
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+#  tags                      = local.common_tags
+#  gateway_id                = aws_internet_gateway.igw.id
+#}
 
 
 #resource "aws_route" "internet_gw_route" {
